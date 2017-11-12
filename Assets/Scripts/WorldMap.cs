0bb7ee0066dbafefdd86d450b4b3abc3d419c714
Dylan Guidry <guidry.dylan.95@gmail.com>
2017-11-12 03:03:39 -0600

    why

---
+++ Assets/Scripts/WorldMap.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldMap : MonoBehaviour
+{
+    public List<LevelSelector> mLevels;
+
+    private void Start()
+    {
+        mLevels.Sort((a, b) => a.mLevelNumber.CompareTo(b.mLevelNumber));
+    }
+
+    private void Update()
+    {
+        if (mLevels[mLevels.Count - 1].mIsComplete)
+            Debug.Log("Vicotry");
+    }
+}
